6aea6048edc8a49c6ab3a510311b28602d559398
lucasaugsantos <lucasaugsantos@gmail.com>
2015-06-30 19:00:37 -0300

    Update bb.h

--- bb.h
+++ bb.h
@@ -1,1 +1,6 @@
+// Funcao que calcula a melhor permuta do algortimo brancdh and bound
+// Input: Entrada de jobs, número total de jobs
+// Output: A melhor permuta
+// Pré-condição: Entrada não nula e número de jobs diferente de 0
+// Pós condição: Melhor permuta de jobs é encontrada
 Permuta* branchAndBound(Job** entrada, int limite, int n);
