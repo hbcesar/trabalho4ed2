7b2d528ccf106a00b43fe3b3db83b4b4ac87b40b
lucasaugsantos <lucasaugsantos@gmail.com>
2015-06-30 19:02:03 -0300

    Update beamsearch.h

--- beamsearch.h
+++ beamsearch.h
@@ -1,1 +1,6 @@
+// Funcao que calcula a melhor permuta utilizando a heuristica beam serach
+// Input: Os jobs de entrada, numero de jobs e tamanho do beam width
+// Output: Estrutura permuta com melhor solucao encontrada pelo beam serach
+// Pré-condição: Jobs não são nulos, width é inteiro positivo
+// Pós condição: Melhor permuta pelo beam search eh encontrada
 Permuta* beamSearch(Job** entrada, int n, int w);
